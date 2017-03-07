5e699853b8d8d324a2d4c340f8938c15d7e6ab4b
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-03-06 23:32:21 -0300

    Prepare Component Structure

--- src/components/SelectField/index.js
+++ src/components/SelectField/index.js
@@ -9,13 +9,19 @@ class SelectField extends React.Component {
     return (
       <div className='select-field'>
         <div className='select-field__input'>
-          <div className='select-field__input__label'>
-            Selecione uma opção
+          <div className='select-field__input-label'>
+            Selecione uma opção...
+          </div>
+          <div className='select-field__input-field'>
+            <input type='text' />
           </div>
         </div>
 
-        <div className='select-field__menu'>
-        </div>
+        <ul className='select-field__menu'>
+          <li className='select-field__menu-item'>Item 1</li>
+          <li className='select-field__menu-item'>Item 2</li>
+          <li className='select-field__menu-item'>Item 3</li>
+        </ul>
       </div>
     )
   }

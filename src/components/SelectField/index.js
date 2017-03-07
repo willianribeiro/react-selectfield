174ce752f1c952e88195a35552f5da8c29785f8e
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-03-06 23:18:35 -0300

    Add SelectField files

---
+++ src/components/SelectField/index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+import React from 'react'
+
+import './SelectField.css'
+
+class SelectField extends React.Component {
+  render () {
+    return (
+      <div className="select-field">
+        <div className="select-field__input">
+          <div className="select-field__input__label">
+            Selecione uma opção
+          </div>
+        </div>
+
+        <div className="select-field__menu">
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SelectField

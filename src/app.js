931190f6aeffb2f5e2eb7279c46e870e004fc6b8
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-03-07 22:53:33 -0300

    Add first css & show and hide options menu

--- src/app.js
+++ src/app.js
@@ -5,10 +5,20 @@ import React, { Component } from 'react'
 import './css/style.css'
 import SelectField from 'components/SelectField'
 
+const options = [
+  {label: 'Option 1', value: 1},
+  {label: 'Option 2', value: 2, isSelected: true},
+  {label: 'Option 3', value: 3}
+]
+
 class App extends Component {
   render () {
     return (
-      <SelectField />
+      <div style={{maxWidth: '300px', margin: '50px auto'}}>
+        <SelectField
+          source={options}
+          className='my-custom-css' />
+      </div>
     )
   }
 }
--- src/components/SelectField/index.js
+++ src/components/SelectField/index.js
@@ -5,22 +5,59 @@ import React from 'react'
 import './SelectField.css'
 
 class SelectField extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      isOpened: false
+    }
+
+    this.toggle = this.toggle.bind(this)
+  }
+
+  toggle () {
+    this.setState({isOpened: !this.state.isOpened})
+  }
+
   render () {
+    let {className, source} = this.props
+
+    className = className ? `${className} select-field` : 'select-field'
+    className += this.state.isOpened ? ' is-opened' : ''
+
     return (
-      <div className='select-field'>
-        <div className='select-field__input'>
-          <div className='select-field__input-label'>
+      <div className={className}>
+        <div
+          className='select-field__display'
+          onClick={this.toggle}
+          onBlur={this.toggle}>
+          <div className='select-field__display-placeholder'>
             Selecione uma opção...
           </div>
-          <div className='select-field__input-field'>
-            <input type='text' />
+          <div className='select-field__display-label'>
+            Opção selecionada
+          </div>
+          <div className='select-field__display-input'>
+            <input type='text' readOnly />
           </div>
         </div>
 
-        <ul className='select-field__menu'>
-          <li className='select-field__menu-item'>Item 1</li>
-          <li className='select-field__menu-item'>Item 2</li>
-          <li className='select-field__menu-item'>Item 3</li>
+        <ul className='select-field__options'>
+          {
+            source.map(item => {
+              let classNameItem
+              classNameItem = 'select-field__option'
+              classNameItem += item.isSelected ? ' is-selected' : ''
+
+              return (
+                <li
+                  key={item.value}
+                  value={item.value}
+                  className={classNameItem}>
+                  {item.label}
+                </li>
+              )
+            })
+          }
         </ul>
       </div>
     )

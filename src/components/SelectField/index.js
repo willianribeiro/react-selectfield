2a53d221c5d8d0a9a6fe0d49553df55984bc5afe
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-03-10 00:09:06 -0300

    Removes console errors from Lint JavaScript

--- src/components/SelectField/index.js
+++ src/components/SelectField/index.js
@@ -127,31 +127,30 @@ class SelectField extends React.Component {
 
     const caretDown = (
       <svg
-        className="select-field__display-caret"
-        fill="#383838"
-        height="24"
-        viewBox="0 0 24 24"
-        width="24"
-        xmlns="http://www.w3.org/2000/svg">
-          <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"/>
-          <path d="M0-.75h24v24H0z" fill="none"/>
+        className='select-field__display-caret'
+        fill='#383838'
+        height='24'
+        viewBox='0 0 24 24'
+        width='24'
+        xmlns='http://www.w3.org/2000/svg'>
+        <path d='M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z' />
+        <path d='M0-.75h24v24H0z' fill='none' />
       </svg>
     )
 
     const clearButton = (
       <svg
-        className="select-field__display-clear"
+        className='select-field__display-clear'
         onClick={this.clear}
-        fill="#383838"
-        height="18"
-        viewBox="0 0 24 24"
-        width="18"
-        xmlns="http://www.w3.org/2000/svg">
-          <path d="M0 0h24v24H0z" fill="none"/>
-          <path d="M14.59 8L12 10.59 9.41 8 8 9.41 10.59 12 8 14.59 9.41 16 12 13.41 14.59 16 16 14.59 13.41 12 16 9.41 14.59 8zM12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/>
+        fill='#383838'
+        height='18'
+        viewBox='0 0 24 24'
+        width='18'
+        xmlns='http://www.w3.org/2000/svg'>
+        <path d='M0 0h24v24H0z' fill='none' />
+        <path d='M14.59 8L12 10.59 9.41 8 8 9.41 10.59 12 8 14.59 9.41 16 12 13.41 14.59 16 16 14.59 13.41 12 16 9.41 14.59 8zM12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z' />
       </svg>
     )
-
 
     return (
       <div className={className}>
@@ -174,8 +173,8 @@ class SelectField extends React.Component {
               ref={(input) => { this.input = input }}
             />
           </div>
-          { !this.state.selected && caretDown }
-          { this.state.selected && clearButton }
+          {!this.state.selected && caretDown}
+          {this.state.selected && clearButton}
         </div>
 
         <ul className='select-field__options'>

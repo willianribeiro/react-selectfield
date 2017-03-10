74768d9cc99f8229f9193b4384b23760d313543c
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-03-09 23:39:17 -0300

    Adds caret down to select field display

--- src/components/SelectField/index.js
+++ src/components/SelectField/index.js
@@ -33,6 +33,19 @@ const events = {
   }
 }
 
+const displayCaret = (
+  <svg
+    className="select-field__display-caret"
+    fill="#383838"
+    height="24"
+    viewBox="0 0 24 24"
+    width="24"
+    xmlns="http://www.w3.org/2000/svg">
+      <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"/>
+      <path d="M0-.75h24v24H0z" fill="none"/>
+  </svg>
+)
+
 class SelectField extends React.Component {
   constructor () {
     super()
@@ -138,6 +151,7 @@ class SelectField extends React.Component {
               ref={(input) => { this.input = input }}
             />
           </div>
+          { displayCaret }
         </div>
 
         <ul className='select-field__options'>

6ee475f3cfaaf35c1c2a67b8eb9db832fab2ae22
Willian Ribeiro <willianribeiro.contato@gmail.com>
2017-03-10 00:05:19 -0300

    Adds clear button

--- src/components/SelectField/index.js
+++ src/components/SelectField/index.js
@@ -33,19 +33,6 @@ const events = {
   }
 }
 
-const displayCaret = (
-  <svg
-    className="select-field__display-caret"
-    fill="#383838"
-    height="24"
-    viewBox="0 0 24 24"
-    width="24"
-    xmlns="http://www.w3.org/2000/svg">
-      <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"/>
-      <path d="M0-.75h24v24H0z" fill="none"/>
-  </svg>
-)
-
 class SelectField extends React.Component {
   constructor () {
     super()
@@ -56,6 +43,7 @@ class SelectField extends React.Component {
 
     this.toggle = this.toggle.bind(this)
     this.close = this.close.bind(this)
+    this.clear = this.clear.bind(this)
     this.handleDocumentClick = this.handleDocumentClick.bind(this)
     this.getDocumentEvents = this.getDocumentEvents.bind(this)
     this.getLabel = this.getLabel.bind(this)
@@ -69,15 +57,22 @@ class SelectField extends React.Component {
     this.setState({isOpened: false})
   }
 
-  handleOptionClick (item) {
+  clear () {
     this.setState(
-      {selected: item},
-      this.handleOptionSelected.bind(this, item)
+      {selected: ''},
+      this.handleOptionSelected.bind(this, '')
     )
   }
 
-  handleOptionSelected (item) {
-    this.props.onChange(item.value)
+  handleOptionClick (item) {
+    this.setState(
+      {selected: item},
+      this.handleOptionSelected.bind(this, item.value)
+    )
+  }
+
+  handleOptionSelected (value) {
+    this.props.onChange(value)
     this.close()
   }
 
@@ -130,6 +125,34 @@ class SelectField extends React.Component {
     className += this.state.isOpened ? ' is-opened' : ''
     className += this.state.selected ? ' is-selected' : ''
 
+    const caretDown = (
+      <svg
+        className="select-field__display-caret"
+        fill="#383838"
+        height="24"
+        viewBox="0 0 24 24"
+        width="24"
+        xmlns="http://www.w3.org/2000/svg">
+          <path d="M7.41 7.84L12 12.42l4.59-4.58L18 9.25l-6 6-6-6z"/>
+          <path d="M0-.75h24v24H0z" fill="none"/>
+      </svg>
+    )
+
+    const clearButton = (
+      <svg
+        className="select-field__display-clear"
+        onClick={this.clear}
+        fill="#383838"
+        height="18"
+        viewBox="0 0 24 24"
+        width="18"
+        xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 0h24v24H0z" fill="none"/>
+          <path d="M14.59 8L12 10.59 9.41 8 8 9.41 10.59 12 8 14.59 9.41 16 12 13.41 14.59 16 16 14.59 13.41 12 16 9.41 14.59 8zM12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/>
+      </svg>
+    )
+
+
     return (
       <div className={className}>
         <div
@@ -151,7 +174,8 @@ class SelectField extends React.Component {
               ref={(input) => { this.input = input }}
             />
           </div>
-          { displayCaret }
+          { !this.state.selected && caretDown }
+          { this.state.selected && clearButton }
         </div>
 
         <ul className='select-field__options'>
